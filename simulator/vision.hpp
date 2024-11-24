15cd0baaee0edbbcc41309b53b31e949e69f41c8
ChanWoo Kim <71691159+kCW-tb@users.noreply.github.com>
2024-11-24 20:34:22 +0900

    Update vision.hpp

--- simulator/vision.hpp
+++ simulator/vision.hpp
@@ -13,4 +13,16 @@ using namespace std;
 Mat pre_image(Mat origin);
 //void ctrlc_handler(int);
 
+class fix_p {
+private:
+    int index;
+    double distance;
+public:
+    fix_p(int index, double distance);
+    int get_index(void);
+    double get_distance();
+};
+
+bool compare_function(fix_p& compair_one, fix_p& compair_two);
+
 #endif
