ab9c6a235cf085c40b153f05031f8dbab0f9fd5a
ChanWoo Kim <71691159+kCW-tb@users.noreply.github.com>
2024-12-02 18:47:21 +0900

    Update vision.hpp

--- moter_sim/vision.hpp
+++ moter_sim/vision.hpp
@@ -13,11 +13,11 @@
 
 using namespace cv;
 using namespace std;
-
+//이미지 전처리
 Mat pre_image(Mat origin);
 
 Mat line_detect(Mat roi);
-
+//라인 선정에 대해 사용하는 fix_p 클래스.
 class fix_p {
 private:
     int index;
@@ -27,11 +27,15 @@ public:
     int get_index(void);
     double get_distance();
 };
-
+//sort함수 오름차순 or 내림차순
 bool compare_function(fix_p& compair_one, fix_p& compair_two);
-
+//error값을 정규화해주는 함수(타 함수에 이용)
 double normalize(int x);
-
+//dxl속도 설정
+void set_dxl(double k, int error);
+//k값 변화하는 함수
 void get_k_error(int error, double *k_val);
+//시스템 종료
+void ctrlc(int);
 
 #endif
